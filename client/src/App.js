fa6fb129a1ff9988d0c6d3fcfe042cc18c8cfe6d
PiotrSzponar <18piotrek06@gmail.com>
2019-10-20 17:38:09 +0200

    Add state, context, reducer of login and registration

--- client/src/App.js
+++ client/src/App.js
@@ -5,27 +5,30 @@ import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 
 import ContactState from './context/contact/ContactState';
+import AuthState from './context/auth/AuthState';
 
 const App = () => {
   return (
-    <ContactState>
-      <Router>
-        <Navbar />
-        <Container fluid>
-          <div
-            style={{
-              padding: '20px 50px',
-              maxWidth: '1200px',
-              margin: '0 auto',
-            }}
-          >
-            <Switch>
-              <Route exact path="/" component={Home} />
-            </Switch>
-          </div>
-        </Container>
-      </Router>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <Router>
+          <Navbar />
+          <Container fluid>
+            <div
+              style={{
+                padding: '20px 50px',
+                maxWidth: '1200px',
+                margin: '0 auto',
+              }}
+            >
+              <Switch>
+                <Route exact path="/" component={Home} />
+              </Switch>
+            </div>
+          </Container>
+        </Router>
+      </ContactState>
+    </AuthState>
   );
 };
 
